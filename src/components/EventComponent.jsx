12bd5a0ed6fd2bb85a81c5c4818fe1cb2a6c1d14
Léo <leogianfagna@gmail.com>
2025-02-03 14:42:14 -0300

    style: ícone único para cada evento

--- src/components/EventComponent.jsx
+++ src/components/EventComponent.jsx
@@ -3,10 +3,20 @@
 import "./EventComponent.css";
 
 const EventComponent = ({ event, i, hour }) => {
+  const eventos = [
+    { name: "contaovelhas", desc: "Descrição sobre o evento" },
+    { name: "matapombo", desc: "Descrição sobre o evento" },
+  ];
+
+  function getEventIcon(eventName) {
+    const iconName = eventName.replace(/\s+/g, '').toLowerCase();
+    return iconName + ".png";
+  }
+
   return (
     <div className="event-div">
       <div className="event-icon">
-        <img src="../src/assets/images/events/contaovelha.png" alt="a"></img>
+        <img src={"../src/assets/images/events/" + getEventIcon(event.lista[i])} alt="a"></img>
       </div>
       <div className="event-name">
         <p>{event.lista[i]}</p>
